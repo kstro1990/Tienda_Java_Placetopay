a93214c09fc3c8ec379b638de1184d80185042d7
EGM\luis.castro <kstrolap@gmail.com>
2021-04-14 23:35:52 -0500

    Se agrega la funcion del carrito de compra

--- froned/src/service/ProductosSerivice.js
+++ froned/src/service/ProductosSerivice.js
@@ -1,6 +1,15 @@
 import axios from 'axios'; 
 
 export default class ProductosSerivice{
+
+    data(){
+        return{
+        customer:0,
+        producto:0,
+        precio:0,
+        cantidad:0
+    }
+    }
     url = 'http://localhost:8080/V1';
     
     getAll(){
@@ -10,6 +19,23 @@ export default class ProductosSerivice{
     addCar(productID){
         var endpoint = 'http://localhost:8080/V1/products/buscar/'+ productID;
         console.log(endpoint);
-        return axios.get(endpoint);
+        var urlAddCard = 'http://localhost:8080/V1/car/guardar';
+        var prductByID = axios.get(endpoint);
+
+        prductByID.then(data=>{
+            var json = {
+                customer: 43,
+                producto: data.data.id,
+                precio: data.data.price,
+                cantidad: 1
+            }
+            var requestCard = axios.post(urlAddCard,json);
+
+            console.log(requestCard);
+        });
+
+        
+
+        return 2;
     }
 }
